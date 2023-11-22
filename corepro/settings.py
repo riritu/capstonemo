036b720211a281b914cb31cb3d517468664a4992
riritu <robertofaner55@gmail.com>
2023-11-22 17:37:24 +0800

    statics

--- corepro/settings.py
+++ corepro/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'coreapp/static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
