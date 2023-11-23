258a6d57871b795838489259fc49dba37cbd39cb
riritu <robertofaner55@gmail.com>
2023-11-23 15:13:35 +0800

    dj

--- corepro/settings.py
+++ corepro/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'corepro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-<<<<<<< HEAD
+
 database_url = os.environ.get("DATABASE_URL")
-=======
+
 
 DATABASES = {
     'default': {
@@ -94,7 +94,6 @@ DATABASES = {
 
 
 
->>>>>>> 103beb59be97c72aba6ed2bb3aa65dbf605982f0
 
 if database_url:
     DATABASES = {'default': dj_database_url.parse(database_url)}
