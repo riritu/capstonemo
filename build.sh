3ada6c61d66d961c0f9448aeb198135a6ccb482a
riritu <robertofaner55@gmail.com>
2023-11-28 18:18:45 +0800

    builds

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 poetry install
 
-RENDER="python manage.py collectstatic --no-input"
-$RENDER
+python manage.py collectstatic --no-input
 python manage.py migrate
