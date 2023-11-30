1629549da12a15dfd9f32ea9f1a37093fd92c3a9
riritu <robertofaner55@gmail.com>
2023-11-30 23:34:44 +0800

    dtbs

--- corepro/settings.py
+++ corepro/settings.py
@@ -25,7 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+if 'RENDER' in os.environ:
+    # If RENDER is present, set DEBUG to False
+    DEBUG = False
+else:
+    # If RENDER is not present, set DEBUG to True
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
