57093a74f9c72da03f3374ba4b85b099d6fe7b48
riritu <robertofaner55@gmail.com>
2023-12-01 23:17:46 +0800

    debetest

--- corepro/settings.py
+++ corepro/settings.py
@@ -83,20 +83,7 @@ WSGI_APPLICATION = 'corepro.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 database_url = os.environ.get("DATABASE_URL")
-
-if database_url:
-    DATABASES = {'default': dj_database_url.parse(database_url)}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'admins',
-            'USER': 'postgres',
-            'PASSWORD': 'renaf',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
-    }
+DATABASES = {'default': dj_database_url.parse(database_url)}
 
 
 
