fbffba1927e7ad699900790c1ee74e4b458c475f
riritu <robertofaner55@gmail.com>
2023-12-02 16:07:33 +0800

    credmin

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 poetry install
 
+python manage.py createadmin
 
 python manage.py collectstatic --no-input
 python manage.py migrate
--- coreapp/management/commands/createadmin.py
+++ coreapp/management/commands/createadmin.py
@@ -1,16 +1,14 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
 
 class Command(BaseCommand):
-    help = 'Create admin user if it does not exist'
+    help = 'Create a generic user'
 
     def handle(self, *args, **options):
-        username = 'trilc'
+        username = 'trilcmean'
+        email = 'trilcenterprises@gmail.com'
         password = 'trilcenterprises'
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create(username=username, password=make_password(password))
-            self.stdout.write(self.style.SUCCESS('Admin user created successfully'))
-        else:
-            self.stdout.write(self.style.SUCCESS('Admin user already exists'))
+        user = User.objects.create_user(username=username, email=email, password=password)
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully created user: {user.username}'))
